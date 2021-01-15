92094081f5076652d65d7e15edef9386d3c7acd6
nevean-15 <nevean15@gmail.com>
2021-01-14 22:12:43 -0500

    Readig all the quotes from text file

--- get-quote.py
+++ get-quote.py
@@ -1,5 +1,4 @@
 def primary():
-   print("Keep it logically awesome.")
 
   f = open("quotes.txt")
   quotes = f.readlines()
@@ -9,5 +8,3 @@ def primary():
 
 if __name__== "__main__":
  primary()
-
-
